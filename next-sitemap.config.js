c354e7c7604b1bd9ba9c3b413fa3b32862f57559
Nhu Le <lekhaquynhnhu2805@gmail.com>
2025-05-18 19:52:43 +0700

    done

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://giaoducsaigon.edu.vn', 
-    generateRobotsTxt: true, // Tự động tạo robots.txt dựa trên sitemap (tùy chọn)
+    generateRobotsTxt: false, // Tự động tạo robots.txt dựa trên sitemap (tùy chọn)
     // Nếu website có nhiều trang (>50k), cần cấu hình thêm split sitemap
     // exclude: ['/server-sitemap.xml'], // Loại trừ sitemap động nếu có
     // robotsTxtOptions: {
